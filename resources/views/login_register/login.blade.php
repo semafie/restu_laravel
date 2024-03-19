b2e6066cb5c76e43f57add368fed994587901ce4
semafie <restuimamsafii04@gmail.com>
2024-03-19 14:23:11 +0700

    cek

---
+++ resources/views/login_register/login.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Restu | login</title>
+</head>
+
+<body>
+    <nav>
+        <img src="" alt="">
+        <div class="link">
+            <a href="">Home</a>
+            <a href="">Sign in</a>
+            <a href="">Sign up</a>
+        </div>
+    </nav>
+</body>
+
+</html>
